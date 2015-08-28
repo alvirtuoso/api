4ff8e42eb52e0d67737fa39477e469c80dcf0422
alvirtuoso <alohunk@yahoo.com>
2015-08-28 13:18:52 -0700

    added php functions

--- index.php
+++ index.php
@@ -29,10 +29,10 @@ $app->configureMode('development', function () use ($app) {
 });
 
 $app->get('/patients', 'getPatients');
-// $app->get('/patients/:id', 'getPatientById');
-// $app->get('/patients/search/:query', 'findByName');
-// $app->get('/patients', 'addPatient');
-// $app->put('/patients/:id', 'updatePatient');
+ $app->get('/patients/:id', 'getPatientById');
+ $app->get('/patients/search/:query', 'findByName');
+ $app->post('/patients', 'addPatient');
+$app->put('/patients/:id', 'updatePatient');
 // $app->delete('/patients/:id', 'deletePatient');
 
 $app->run();
@@ -53,5 +53,94 @@ function getPatients()
         echo '{"error":{"text":'. $e->getMessage() .'}}';
     } 
 }
+
+function getPatientById($id)
+{ 
+  // echo "{:id}";
+  //if(!IsNullOrEmptyString($id)){
+
+      $sql = "SELECT * FROM patient_data WHERE id = :id";
+    try {
+      $db = getDB();
+      $stmt = $db->prepare($sql);
+      $query = "%".$id."%";
+      $stmt->bindParam("id", $id);
+      $stmt->execute();
+      $patient = $stmt->fetchObject();
+      $db = null;
+        echo json_encode($patient);
+    } catch (PDOException $e) {
+      echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+    }
+  //}
+
+}
+
+function findByName($query)
+{
+  //echo 'finding name ';
+  $sql = "SELECT * FROM patient_data WHERE UPPER(lname) LIKE :query || UPPER(fname) LIKE :query ORDER BY lname";
+  try {
+    $db = getDB();
+    $stmt = $db->prepare($sql);
+    $query = "%".$query."%";
+    $stmt->bindParam("query", $query);
+    $stmt->execute();
+    $patients = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $db = null;
+      echo '{"patient": ' . json_encode($patients) . '}';
+  } catch (PDOException $e) {
+    echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+  }
+}
+
+function addPatient()//$fname, $lname, $dob, $sex) 
+{
+  echo "hello";
+     $request = \Slim\Slim::getInstance()->request();
+     $patient = json_decode($request->getBody());
+     var_dump($patients);
+    // $sql = "INSERT INTO patient_data (fname, lname, DOB, sex) VALUES (:fname, :lname, :dob, :sex)";
+    // try {
+    //     $db = getConnection();
+    //     $stmt = $db->prepare($sql);
+    //     $stmt->bindParam("name", $patient->name);
+    //     $stmt->bindParam("grapes", $patient->grapes);
+    //     $stmt->bindParam("country", $patient->country);
+    //     $stmt->bindParam("region", $patient->region);
+    //     $stmt->bindParam("year", $patient->year);
+    //     $stmt->bindParam("description", $patient->description);
+    //     $stmt->execute();
+    //     $patient->id = $db->lastInsertId();
+    //     $db = null;
+    //     echo json_encode($patient);
+    // } catch(PDOException $e) {
+    //     echo '{"error":{"text":'. $e->getMessage() .'}}';
+    // }
+}
+
+function updateWine($id) {
+    $request = \Slim\Slim::getInstance()->request();
+    $body = $request->getBody();
+    $patient = json_decode($body);
+    $sql = "UPDATE wine SET name=:name, grapes=:grapes, country=:country, region=:region, year=:year, description=:description WHERE id=:id";
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("name", $patient->name);
+        $stmt->bindParam("grapes", $patient->grapes);
+        $stmt->bindParam("country", $patient->country);
+        $stmt->bindParam("region", $patient->region);
+        $stmt->bindParam("year", $patient->year);
+        $stmt->bindParam("description", $patient->description);
+        $stmt->bindParam("id", $id);
+        $stmt->execute();
+        $db = null;
+        echo json_encode($patient);
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
+
 
 ?>
